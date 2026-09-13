4e946cc866d9490ca24243291a5b123193fdce89
acencoff22-rgb <acencoff22@gmail.com>
2026-09-14 07:45:15 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,56 @@
+"use strict";
+
+export const STORAGE_KEY =
+  "assignment-app-data-v6";
+
+export const GITHUB_CONFIG_KEY =
+  "assignment-app-github-config-v1";
+
+export const THEME_KEY =
+  "assignment-app-theme";
+
+export const DATA_VERSION = 7;
+
+export const RETENTION_MONTHS = 6;
+
+export const BEAM_WIDTH = 3000;
+
+export const MAX_STATES_PER_SIGNATURE = 2;
+
+
+export const WORKERS = [
+  "김",
+  "탁",
+  "임",
+  "박",
+  "류",
+];
+
+
+export const JOBS = [
+  "볼분리",
+  "볼분리 보조",
+  "설거지 및 성형보조",
+  "분쇄 및 성형보조",
+  "성형 및 분쇄보조",
+];
+
+
+export const MAIN_WORKERS = [
+  "김",
+  "탁",
+  "임",
+];
+
+
+export const LIU_JOBS = [
+  "설거지 및 성형보조",
+  "분쇄 및 성형보조",
+  "성형 및 분쇄보조",
+];
+
+
+export const PARK_ALLOWED_JOBS = [
+  "설거지 및 성형보조",
+  "분쇄 및 성형보조",
+];
